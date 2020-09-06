3178c2d1d844f44415e1aeead28c9620fefedf7a
kagomesan <65454462+kagomesan@users.noreply.github.com>
2020-09-06 21:31:21 +0900

    Update get-config.py

--- get-config.py
+++ get-config.py
@@ -7,12 +7,19 @@ sys.path.append('/usr/lib/python3.8/site-packages')
 import pexpect
 import getpass
 
-id = raw_input('id: ')
-pw = getpass.getpass()
+#id = raw_input('id: ')
+#pw = getpass.getpass()
 
 f = open('list.txt', 'r')
+c = open('cmd.txt', 'r')
+
+const = ["ID","PASS"]
 
 for line in f:
+
+    line = line.strip()
+    begin = line + ' connect .. '
+    sys.stdout.write(begin)
 
     child = pexpect.spawn('telnet ' + line)
     
@@ -20,24 +27,27 @@ for line in f:
                    u'login:']
 
     child.expect(expect_list)
-    child.sendline(id)
+    child.sendline(const[0])
 
     child.expect('Password:')
-    child.sendline(pw)
+    child.sendline(const[1])
 
-    line = line.strip()
     str = line + ' #'
     child.expect(str)
-    child.sendline('get system session status')
 
-    fn = line + '_result.txt'
-    spath = os.path.join('./log',fn)
-    fout = open( spath,'w')
-    child.logfile = fout
+    for word in c:
+        word = word.split(",")
+        child.sendline(word[1])
+        fn = line + '_' + word[0] + '.txt'
+        spath = os.path.join('./log',fn)
+        fout = open( spath,'w')
+        child.logfile = fout
+        child.expect(str)
+    c.close()
 
     child.expect(str)
     child.sendline('exit\n')
     child.close()
     child.logfile.close()
-    print line + ' done'
-f.close() 
+    print ' done'
+f.close()
